3a65bdcdb0f10907b760e5c4ec89fb3397322f93
Miguel M <miguelmiguelezcazapal13@gmail.com>
2022-09-16 10:33:22 +0200

    16/09/2022_10:33

---
+++ 01-intro-to-c#/06-exercise/Ejercicio6/Program.cs
@@ -0,0 +1,52 @@
+﻿#define DIR
+namespace Ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f = 45;
+            int i = 75;
+#if !DIR
+            calcFactorial(45, ref f);
+            System.Console.WriteLine("Result: " + f);
+#else
+            paintAsterisk();
+#endif
+            Console.ReadKey();
+        }
+
+
+        static Boolean calcFactorial(int n, ref double facto)
+        {
+            facto = 1;
+            for (int i = n; i > 0; i--)
+            {
+                facto *= i;
+            }
+
+            if (facto < 0 || facto > 10)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static void paintAsterisk(int n = 10)
+        {
+            Random random = new Random();
+            int auxCol;
+            int auxRow;
+
+            for (int i = 0; i < n; i++)
+            {
+                auxCol = random.Next(1, 21);
+                auxRow = random.Next(1, 21);
+
+                Console.WriteLine("*");
+                Console.SetCursorPosition(auxCol, auxRow);
+            }
+        }
+
+    }
+}

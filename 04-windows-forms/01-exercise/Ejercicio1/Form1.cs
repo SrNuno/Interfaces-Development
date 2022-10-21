6dea7ade6a6a2a7913c561187b32e9ed72d1c63c
Miguel M <miguelmiguelezcazapal13@gmail.com>
2022-10-21 15:55:05 +0200

    21/10/2022_15:54

---
+++ 04-windows-forms/01-exercise/Ejercicio1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        const string text = "Form1";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender.GetType() == typeof(Button))
+            {
+                this.Text = $"X: {e.X + ((Button)sender).Location.X}, Y: {e.Y + ((Button)sender).Location.Y}";
+            }
+            else
+            {
+                this.Text = $"X: {e.X}, Y: {e.Y}";
+            }
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Text = text;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
